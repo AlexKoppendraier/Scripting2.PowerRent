e858fa5e2108c4ab8c2d94de70657152e2b67003
Oninnaise <Oninnaise@users.noreply.github.com>
2017-01-24 11:53:26 +0100

    Add files via upload

--- game.py
+++ game.py
@@ -1,41 +1,8 @@
 import pygame
 import os   # regelt filesystem voor get_image
-<<<<<<< HEAD
-import psycopg2
-
-# Connect to an existing database
-conn = psycopg2.connect("dbname=project2 user=postgres password=181aea6e")
-
-# Open a cursor to perform database operations
-cur = conn.cursor()
-
-# Execute a command: this creates a new table
-#cur.execute("CREATE TABLE players (name varchar(10) PRIMARY KEY, wins integer, losses integer);")
-
-# Pass data to fill a query placeholders and let Psycopg perform
-# the correct conversion (no more SQL injections!)
-
-#cur.execute("INSERT INTO players (name,wins, losses) VALUES (%s, %s, %s)",
-#('test', 10, 2))
-
-# Query the database and obtain data as Python objects
-
-#cur.execute("SELECT * FROM players")
-#cur.fetchone()
-#(1, 100, "abc'def")
-
-# Make the changes to the database persistent
-conn.commit()
-
-# Close communication with the database
-cur.close()
-conn.close()
-
-=======
 import random
 import sys
 import eztext
->>>>>>> origin/master
 pygame.init()
 display_width = 800
 display_height = 600
@@ -106,21 +73,6 @@ def button(msg,x,y,w,h,ic,ac,action=None):          #functie om een knop te make
     textRect.center = ( (x+(w/2)), (y+(h/2)) )
     gameDisplay.blit(textSurf, textRect)
 
-def dice(x,y):
-    part = 0
-    if part == 0:
-        result = str(random.randint(x,y))
-        part = part + 1
-    while part <= 180:
-        largeText = pygame.font.SysFont("freesansbold.ttf", 115)
-        TextSurf, TextRect = text_objects(result, largeText)
-        TextRect.center = ((display_width / 2), (display_height / 4))
-        gameDisplay.blit(TextSurf, TextRect)
-        part = part + 1
-        print(part)
-        pygame.display.flip()
-    else:
-        return
 def text_objects(text, font):   #functie om tekst te tonen
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
@@ -162,15 +114,8 @@ def game_instructions():    #instructie scherm
                 pygame.quit()
                 quit()
         gameDisplay.fill(white)
-        gameDisplay.blit(get_image('euromast1.png'), (0, 0))
-        gameDisplay.blit(get_image('euromast2.png'), (0, 0))
-        gameDisplay.blit(get_image('euromast3.png'), (0, 0))
-        gameDisplay.blit(get_image('euromast4.png'), (0, 0))
         button("Back", 50, 500, 700, 50, tint_green, green, game_intro)
         clock.tick(15)  #refresh rate van 15
-        if rolling == True:
-            dice(1,6)
-            rolling = False
         pygame.display.flip()
 
 def players(): #speler keuze scherm
@@ -207,9 +152,5 @@ def game_main():    #hoofd gamescherm
 game_intro()
 game_instructions()
 game_main()
-<<<<<<< HEAD
-quit()
-=======
 players()
 quit()
->>>>>>> origin/master
